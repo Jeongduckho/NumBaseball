59f9c50cccde004c899ada37f3a5db839874fa55
Jeongduckho <Jeongduckho@users.noreply.github.com>
2014-08-08 02:13:55 +0900

    porting ( 3/x )
    랜덤수를 만든후 사용자에게 입력받는 함수들을 포팅하였습니다.

--- program.cs
+++ program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Numbaseball {
 	class Program {
+		public static int tempcode;
 		static void mv() {
 			Console.Write("     ");
 		}
@@ -15,6 +16,52 @@ namespace Numbaseball {
 				Console.Write(symbol);
 			}
 			Console.WriteLine();
+		}
+		static void mkrandnum(int i) {
+			int p, q = 0;
+			Random r = new Random(DateTime.Now.Millisecond);
+			int[] n = new int[i + 1];
+			n[0] = r.Next(0, 9);
+			for (p = 1; p <= i; p++) {
+				n[p] = r.Next(0, 9);
+				for (q = 0; q < p; q++) {
+					if (n[p] == n[q] | n[0] == n[p]) {
+						p--;
+						break;
+					}
+				}
+			}
+		}
+		static void getusernum(int i) {
+			int p, q;
+			int[] user = new int[i + 1];
+			Console.WriteLine();
+			if (tempcode != 1) {
+				mv();
+				printline('-', 46);
+				Console.WriteLine("     자리수 만큼 한 자리씩 숫!자!로! 입력해주세요.");
+				mv();
+				printline('-', 46);
+				Console.WriteLine();
+			}
+			for (p = 0; p <= i; p++) {
+				Console.Write("     {0}의 자리 : ", i + 1 - p);
+				user[p] = Convert.ToInt32(Console.ReadLine());
+				if (user[p] >= 10 | char.IsDigit((char) user[p]) == true) {
+					Console.WriteLine("     10미만의 수만 입력가능합니다.");
+					p--;
+				}
+				if (char.IsDigit((char) user[p]) != true) {
+					for (q = 0; q <= p; q++) {
+						if (user[p] == user[q] && p != q) {
+							Console.WriteLine("     숫자가 중복되었습니다.");
+							Console.WriteLine("      다른 수를 넣어주세요.");
+							p--;
+							break;
+						}
+					}
+				}
+			}
 		}
 		static void Main(string[] args) {
 			int i;
@@ -50,6 +97,8 @@ namespace Numbaseball {
 			if (i == 0) {
 				return;
 			}
+			mkrandnum(i);
+			getusernum(i);
 			Console.ReadLine();
 		}
 	}
